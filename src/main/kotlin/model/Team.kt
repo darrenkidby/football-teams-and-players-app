6470d3bc8c0c3af1d5fe180e3796b89f63559a39
Darren <20088411@mail.wit.ie>
2022-11-16 15:47:55 +0000

    Add Model class called Team

---
+++ src/main/kotlin/model/Team.kt
@@ -0,0 +1,8 @@
+package models
+
+data class Note(val teamName: String,
+                val teamCountry: String,
+                val league: String,
+                val leaguePosition: Int,
+                val isTeamPlayingEurope :Boolean){
+}
