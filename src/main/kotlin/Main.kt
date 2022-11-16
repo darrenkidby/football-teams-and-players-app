3fece5718dd9321f6a5e605d97431586bca48efb
Darren <20088411@mail.wit.ie>
2022-11-16 14:59:13 +0000

    Add menu for teams

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,60 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.System.exit
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+val scanner = Scanner(System.`in`)
+
+fun main(args: Array<String>) {
+    runMenu()
+}
+
+fun mainMenu() : Int {
+    print("""
+          ----------------------------------
+          |       FOOTBALL TEAM APP        |
+          ----------------------------------
+          | TEAM MENU                      |
+          |   1) Add a team                |
+          |   2) List all teams            |
+          |   3) Update a team             |
+          |   4) Remove a team             |
+          ----------------------------------
+          |   0) Exit                      |
+          ----------------------------------
+          ==>> """)
+    return scanner.nextInt()
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1  -> addTeam()
+            2  -> listTeams()
+            3  -> updateTeam()
+            4  -> removeTeam()
+            0  -> exitApp()
+            else -> println("Invalid option entered: " + option)
+        }
+    } while (true)
+}
+
+fun addTeam(){
+    println("You chose Add Note")
+}
+
+fun listTeams(){
+    println("You chose List Notes")
+}
+
+fun updateTeam(){
+    println("You chose Update Note")
+}
+
+fun removeTeam(){
+    println("You chose Delete Note")
+}
+
+fun exitApp(){
+    println("Exiting...bye")
+    exit(0)
 }
