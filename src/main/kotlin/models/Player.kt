2757219eaf4af93ca6a12c389459061d6b766800
Darren <20088411@mail.wit.ie>
2022-12-04 19:20:00 +0000

    Update Team.kt to allow Player.kt to run with it

--- src/main/kotlin/models/Player.kt
+++ src/main/kotlin/models/Player.kt
@@ -1,9 +1,11 @@
 package models
 
-data class Player(  var playerName: String,
+data class Player(  var playerId: Int = 0,
+                    var playerName: String,
                     var playerAge: Int,
                     var playerPosition: String,
                     var playerCost: String,
                     var playerWage: String,
                     var isPlayerYouth: Boolean = false,
-                    var isPlayerRetired: Boolean = false)
+                    var isPlayerRetired: Boolean = false){
+}
